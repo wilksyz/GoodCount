958741a04f7d4fa0f45715b2d55cc96e34193602
wilksyz <antoine-p@hotmail.com>
2019-05-15 18:15:30 +0200

    Add SignInActivity

---
+++ app/src/main/java/com/antoine/goodCount/ui/SignInActivity.kt
@@ -0,0 +1,13 @@
+package com.antoine.goodCount.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.antoine.goodCount.R
+
+class SignInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
