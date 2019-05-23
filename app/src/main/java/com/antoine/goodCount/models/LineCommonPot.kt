59d999ce2421f431a681db5b66d0c65bbe868b69
wilksyz <antoine-p@hotmail.com>
2019-05-23 17:16:52 +0200

    Add LineCommonPot class

---
+++ app/src/main/java/com/antoine/goodCount/models/LineCommonPot.kt
@@ -0,0 +1,13 @@
+package com.antoine.goodCount.models
+
+import com.google.firebase.firestore.ServerTimestamp
+import java.util.*
+
+class LineCommonPot(var id: String?,
+                    var commonPotId: String,
+                    var title: String,
+                    var amount: Double,
+                    @ServerTimestamp var date: Date,
+                    var payedBy: String) {
+    constructor():this("", "","",0.0, Date(), "")
+}
