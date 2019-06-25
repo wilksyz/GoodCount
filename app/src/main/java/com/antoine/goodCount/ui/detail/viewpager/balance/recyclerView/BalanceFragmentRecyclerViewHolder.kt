9d2c43732bc23e37f090ca2e9677f82537521f13
wilksyz <antoine-p@hotmail.com>
2019-06-25 15:49:30 +0200

    Improve the UI of BalanceFragment

--- app/src/main/java/com/antoine/goodCount/ui/detail/viewpager/balance/recyclerView/BalanceFragmentRecyclerViewHolder.kt
+++ app/src/main/java/com/antoine/goodCount/ui/detail/viewpager/balance/recyclerView/BalanceFragmentRecyclerViewHolder.kt
@@ -1,7 +1,9 @@
 package com.antoine.goodCount.ui.detail.viewpager.balance.recyclerView
 
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.antoine.goodCount.R
 import com.antoine.goodCount.models.Participant
 import kotlinx.android.synthetic.main.recyclerview_fragment_balance.view.*
 import java.text.NumberFormat
@@ -13,7 +15,14 @@ class BalanceFragmentRecyclerViewHolder(itemView: View): RecyclerView.ViewHolder
         val format = NumberFormat.getCurrencyInstance(Locale.getDefault())
         format.currency = Currency.getInstance(currency)
         val balanceFormat = format.format(balance)
-        val lineString = "${participant.username}: $balanceFormat"
-        itemView.balance_recyclerview_textView.text = lineString
+        itemView.balance_username_recyclerview_textView.text = participant.username
+        itemView.balance_amount_recyclerview_textView.text = balanceFormat
+        if (balance != null) {
+            if (balance < 0){
+                itemView.container_balance_recycler_view.background = ContextCompat.getDrawable(itemView.context, R.drawable.custom_background_negative)
+            }else {
+                itemView.container_balance_recycler_view.background = ContextCompat.getDrawable(itemView.context, R.drawable.custom_background_positive)
+            }
+        }
     }
 }
